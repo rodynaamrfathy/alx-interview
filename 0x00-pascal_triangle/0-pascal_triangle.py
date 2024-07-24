ecf5d21bcd85c52b25b56c7ac46d0ba7de9dd8be
rodynaamrfathy <rodynaamr@icloud.com>
2024-07-24 11:56:58 +0000

    README.md file update

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""pascal triangle function that returns a list of lists of integers representing the Pascal’s triangle of n"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
